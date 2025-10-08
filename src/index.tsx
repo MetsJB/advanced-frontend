e24206268258ae38764ea617a2d5bb383f6e89c2
Zahar <zaharmaticev@gmail.com>
2025-10-08 20:59:55 +1000

    add vite for development mode

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import { ErrorBoundary } from "@/app/providers/ErrorBoundary";
 import { StoreProvider } from "@/app/providers/StoreProvider";
 import "./app/styles/index.scss";
 import "@/shared/config/i18n/i18n";
+
+
 const container = document.getElementById("root");
 
 if (!container) {
