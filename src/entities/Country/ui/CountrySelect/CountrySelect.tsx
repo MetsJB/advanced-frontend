d48d9904dcc0d33b3ed4d270183bf78638e6b848
Zahar <zaharmaticev@gmail.com>
2025-10-18 07:05:43 +1000

    update custom eslint plugin, add auto fix for oublic api imports

--- src/entities/Country/ui/CountrySelect/CountrySelect.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.tsx
@@ -11,37 +11,35 @@ interface CountrySelectProps {
 }
 
 const options = [
-    { value: Country.Russia, content: Country.Russia },
-    { value: Country.Belarus, content: Country.Belarus },
-    { value: Country.Armenia, content: Country.Armenia },
-    { value: Country.Kazakhstan, content: Country.Kazakhstan },
+  { value: Country.Russia, content: Country.Russia },
+  { value: Country.Belarus, content: Country.Belarus },
+  { value: Country.Armenia, content: Country.Armenia },
+  { value: Country.Kazakhstan, content: Country.Kazakhstan },
 ];
 
 export const CountrySelect = memo(
-    ({
-        className, onChange, value, readonly,
-    }: CountrySelectProps) => {
-        const { t } = useTranslation();
+  ({ className, onChange, value, readonly }: CountrySelectProps) => {
+    const { t } = useTranslation();
 
-        const onChangeHandler = useCallback(
-            (value: string) => {
-                onChange?.(value as Country);
-            },
-            [onChange],
-        );
+    const onChangeHandler = useCallback(
+      (value: string) => {
+        onChange?.(value as Country);
+      },
+      [onChange]
+    );
 
-        return (
-            <ListBox
-                onChange={onChangeHandler}
-                className={className}
-                defaultValue={t('Укажите страну')}
-                items={options}
-                readonly={readonly}
-                value={value}
-                direction="top right"
-                label={t('Укажите страну')}
-            />
-        );
+    return (
+        <ListBox
+          onChange={onChangeHandler}
+          className={className}
+          defaultValue={t('Укажите страну')}
+          items={options}
+          readonly={readonly}
+          value={value}
+          direction='top right'
+          label={t('Укажите страну')}
+        />
+    );
 
     // return (
     //   <Select
@@ -53,5 +51,5 @@ export const CountrySelect = memo(
     //     readonly={readonly}
     //   />
     // );
-    },
+  }
 );
