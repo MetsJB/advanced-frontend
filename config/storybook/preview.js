61842c3a145abb5f468888f893bc727d001eb83c
Zahar <zaharmaticev@gmail.com>
2025-10-19 18:27:25 +1000

    fis unit with aliases, fix storybook after refactoring, aupdate screens, disabled github workflows

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,17 +1,19 @@
-import { addDecorator } from "@storybook/react";
-import { StyleDecorator } from "../../src/shared/config/storybook/styleDecorator/StyleDecorator";
-import { ThemeDecorator } from "../../src/shared/config/storybook/themeDecorator/ThemeDecorator";
-import { RouterDecorator } from "./../../src/shared/config/storybook/routerDecorator/RouterDecorator";
-import { SuspenseDecorator } from "./../../src/shared/config/storybook/SuspenceDecorator/SuspenceDecorator";
-import { Theme } from "../../src/app/providers/ThemeProvider";
+import { addDecorator } from '@storybook/react';
+import { StyleDecorator } from '../../src/shared/config/storybook/styleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/themeDecorator/ThemeDecorator';
+import { RouterDecorator } from '../../src/shared/config/storybook/routerDecorator/RouterDecorator';
+import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenceDecorator/SuspenceDecorator';
+import { Theme } from '../../src/shared/const/theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
+  layout: 'fullscreen'
 };
 
 addDecorator(StyleDecorator);
